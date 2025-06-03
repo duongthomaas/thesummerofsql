36c2964b107f30037cc7a042cbf66cb9485d28ed
Thomas Duong <thomas.duong@theinformationlab.co.uk>
2025-06-03 15:37:56 +0100

    Update foodie_fi_sections_a_b.sql

--- Solutions/foodie_fi_sections_a_b.sql
+++ Solutions/foodie_fi_sections_a_b.sql
@@ -18,16 +18,16 @@ having customer_id <= 19
 order by customer_id, start_date asc
 ;
 
-// B. Data Analysis Questions
+-- // B. Data Analysis Questions
 
-// 1. How many customers has Foodie-Fi ever had?
+-- // 1. How many customers has Foodie-Fi ever had?
 
 select
     count (distinct cast (customer_id as text)) as count_of_customers
 from subscriptions
 ;
 
-// 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+-- // 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
 
 select 
     date_trunc ('month', start_date) as truncated_month ,
@@ -38,7 +38,7 @@ group by date_trunc ('month', start_date)
 order by date_trunc ('month', start_date) asc
 ;
 
-// 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+-- // 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
 
 select
     plan_name ,
@@ -50,7 +50,7 @@ where date_part ('year', start_date) > 2020
 group by plan_name
 ;
 
-// 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+-- // 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
 
 select
     round (count (distinct customer_id) / (select count (distinct customer_id) from subscriptions) * 100, 1) as count_of_customers_churned
@@ -58,7 +58,7 @@ from subscriptions
 where plan_id = 4
 ;
 
-// 5. How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+-- // 5. How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
 
 with cte as (
     select
@@ -78,9 +78,9 @@ select
 from cte
 where trial_plan = true and churned = true 
 ;
-// The way I interpret the question is that we are looking for customers who first had trial subscription (plan_id = 0)
-// and right after the trial subscription churned (plan_id = 4).
-// There is no record meeting those requirements. Let's broaden the scope to any subscription with a subsequent churn.
+-- The way I interpret the question is that we are looking for customers who first had trial subscription (plan_id = 0)
+-- and right after the trial subscription churned (plan_id = 4).
+-- There is no record meeting those requirements. Let's broaden the scope to any subscription with a subsequent churn.
 
 with cte as (
     select
@@ -97,7 +97,7 @@ from cte
 where plan_id = 4 and rnk = 2
 ;
 
-// 6. What is the number and percentage of customer plans after their initial free trial?
+-- // 6. What is the number and percentage of customer plans after their initial free trial?
 
 with cte as (
     select
@@ -118,7 +118,7 @@ where rnk = 2
 group by plan_name
 ;
 
-// 7. What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+-- // 7. What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
 
 with cte as (
     select
@@ -140,7 +140,7 @@ where rnk = 1
 group by plan_name
 ;
 
-// 8. How many customers have upgraded to an annual plan in 2020?
+-- // 8. How many customers have upgraded to an annual plan in 2020?
 
 select
     count (distinct customer_id) as count_of_customers_with_annual_subs
@@ -148,7 +148,7 @@ from subscriptions
 where date_part ('year', start_date) = 2020 and plan_id = 3
 ;
 
-// 9. How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+-- // 9. How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
 
 with trial as (
     select
@@ -171,7 +171,7 @@ inner join annual as a
 on t.customer_id = a.customer_id
 ;
 
-// 10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+-- // 10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
 
 with trial as (
     select
@@ -209,7 +209,7 @@ inner join annual as a
 group by bin 
 ;
 
-// 11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+-- // 11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
 
 with pro_monthly as (
     select
